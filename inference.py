9e8aab4105fcd1f5263b3eb6f980ec3f09cdd305
Rayk <92890505+rayklenz@users.noreply.github.com>
2026-03-24 20:45:10 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -232,8 +232,8 @@ def main(cfg: DictConfig):
                     num_inference_steps=NUM_INFERENCE_STEPS,
                     guidance_scale=GUIDANCE_SCALE,
                     negative_prompt=style["negative"],
-                    height=512,
-                    width=512,
+                    height=768,
+                    width=768,
                     cross_attention_kwargs={"scale": LORA_STRENGTH}
                 ).images[0]
 
